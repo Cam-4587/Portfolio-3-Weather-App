1389ec286a0cdd17f11ec3111f3059c56355beb5
Cam-4587 Conroy <cameronconroy@live.com>
2024-06-24 09:45:30 +0000

    If/else sequence added to create an error message if the city isn't found or to extract the cities weather and temperature

--- run.py
+++ run.py
@@ -6,3 +6,11 @@ city_input = input("Enter city: ")
 
 weather_data = requests.get (f"https://api.openweathermap.org/data/2.5/weather?q={city_input}&units=imperial&APPID={api_key}")
 
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {city_input} is: {weather}")
+    print(f"The temperature in {city_input} is: {temp}ºF")
