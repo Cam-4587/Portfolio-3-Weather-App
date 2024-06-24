ca908b629568d319647b6c28023abcb01b33b9a5
Cam-4587 Conroy <cameronconroy@live.com>
2024-06-24 10:37:17 +0000

    imported datetime module, expanded else statement to include what temperature feels like, wind speed, humidity, description, sunrise and sunset

--- run.py
+++ run.py
@@ -1,16 +1,33 @@
+import datetime as dt
 import requests
 
 api_key = '30d4741c779ba94c470ca1f63045390a'
 
 city_input = input("Enter city: ")
 
-weather_data = requests.get (f"https://api.openweathermap.org/data/2.5/weather?q={city_input}&units=imperial&APPID={api_key}")
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_input}&units=imperial&APPID={api_key}").json()
 
-if weather_data.json()['cod'] == '404':
+if weather_data['cod'] == '404':
     print("No City Found")
 else:
-    weather = weather_data.json()['weather'][0]['main']
-    temp = round(weather_data.json()['main']['temp'])
+    weather = weather_data['weather'][0]['main']
+    temp = round(weather_data['main']['temp'])
+    temp_celsius = round((temp-32) * 5.0/9.0)
+    feels_like_F = round(weather_data['main']['feels_like'])
+    feels_like_C = round((feels_like_F - 32) * 5.0/9.0)
+    wind_speed = round(weather_data['wind']['speed'])
+    humidity = weather_data['main']['humidity']
+    description = weather_data['weather'][0] ['description']
+    sunrise = dt.datetime.fromtimestamp(weather_data['sys']['sunrise'])
+    sunset = dt.datetime.fromtimestamp(weather_data['sys']['sunset'])
 
     print(f"The weather in {city_input} is: {weather}")
-    print(f"The temperature in {city_input} is: {temp}ºF")
+    print(f"The temperature in {city_input} is: {temp}ºF or {temp_celsius}ºC")
+    print(f"The temperature feels like: {feels_like_F}ºF or {feels_like_C}ºC")
+    print(f"The wind speed in {city_input} is: {wind_speed} mph")
+    print(f"The humidiy in {city_input} is: {humidity}%")
+    print(f"The description in {city_input} is: {humidity}%")
+    print(f"The sunrise in {city_input} is: {sunrise}")
+    print(f"The sunset in {city_input} is: {sunset}")
+
+
