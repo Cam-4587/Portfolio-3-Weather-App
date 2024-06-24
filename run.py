e381ca002e5e9dc3a06fccea80ed94237f7a38e3
Cam-4587 Conroy <cameronconroy@live.com>
2024-06-24 09:28:36 +0000

    requestsmodule imported, api_key variable created, city_input added and data request from openweathermap created

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+
+api_key = '30d4741c779ba94c470ca1f63045390a'
+
+city_input = input("Enter city: ")
+
+weather_data = requests.get (f"https://api.openweathermap.org/data/2.5/weather?q={city_input}&units=imperial&APPID={api_key}")
 
