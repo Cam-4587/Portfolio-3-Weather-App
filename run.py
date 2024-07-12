72ca10f6445fc064ff4be38fea40e12eaa517b23
Cameron Conroy <cameronconroy@live.com>
2024-07-12 15:38:04 +0000

    Make changes to remove pep8ci errors except 'line too long errors'

--- run.py
+++ run.py
@@ -10,26 +10,26 @@ import re
 from replit import clear
 import pycountry
 
-#Load environment variables from .env file
-load_dotenv() 
+# Load environment variables from .env file
+load_dotenv()
 api_key = (os.getenv("API_KEY"))
 
-#Print welcome message to the user
+# Print welcome message to the user
 print("Welcome to the Weather App! This app will allow you to search for the weather in any city in the world, and pollen data for selected cities.")
 
-#Start an infiinite loop to keep the program running
+# Start an infiinite loop to keep the program running
 while True:
     # Print the options for the user
     print("Press 1 for current weather\nPress 2 for 5 day forecast\nPress 3 for current Pollen data (data available in Europe as far eastwards as\nArmenia and including North Africa)\nPress 4 for historical data going as far back as 1940")
-    
+
     # Get the user's choice
     choice = input("Enter your choice: ")
 
-    #validate the user's choice
+    # validate the user's choice
     while choice not in ['1', '2', '3', '4']:
         print("Invalid choice. Please enter 1,2,3 or 4.")
         choice = input("Enter your choice: ")
-    #handle the user's choice
+    # handle the user's choice
     else:
         if choice == '1':
             # Get the current weather for a city
@@ -48,7 +48,6 @@ while True:
                 if response.status_code == 404 or not data:
                     print("City not found. Please enter a valid city name and country.")
                     continue
-                
                 lat = data[0]['lat']
                 lon = data[0]['lon']
 
@@ -68,19 +67,21 @@ while True:
                     feels_like_C = round((feels_like_F - 32) * 5.0/9.0)
                     wind_speed = round(weather_data['wind']['speed'])
                     humidity = weather_data['main']['humidity']
-                    description = weather_data['weather'][0] ['description']
+                    description = weather_data['weather'][0]['description']
                     rain = weather_data.get('rain', {'3h': 0})
                     timezone_offset = weather_data['timezone']
                     sunrise = dt.datetime.fromtimestamp(weather_data['sys']['sunrise'] + timezone_offset)
                     sunset = dt.datetime.fromtimestamp(weather_data['sys']['sunset'] + timezone_offset)
                     # Print the weather data for that city
-                    print("\n"*3)
+                    print("\n" * 3)
                     print("-------------------------------------------------------")
                     print(f"The weather in {city_input} is: {weather}")
                     print("-------------------------------------------------------")
-                    #option to print latitude and longitude for city
+                    """
+                    option to print latitude and longitude for city
                     print(f"Latitude: {lat}, Longitude: {lon}")
                     print("-------------------------------------------------------")
+                    """
                     print(f"The temperature in {city_input} is: {temp}ºF or {temp_celsius}ºC\n")
                     print(f"The temperature feels like: {feels_like_F}ºF or {feels_like_C}ºC\n")
                     print(f"The wind speed in {city_input} is: {wind_speed} mph\n")
@@ -108,7 +109,6 @@ while True:
                 if response.status_code == 404 or not data:
                     print("City not found. Please enter a valid city name.")
                     continue
-
                 lat = data[0]['lat']
                 lon = data[0]['lon']
 
@@ -121,12 +121,14 @@ while True:
                     continue
                 # Extract the 5 day weather forecast data for that city
                 else:
-                    print("\n"*3)
+                    print("\n" * 3)
                     print("-------------------------------------------------------")
                     print(f"5-Day Weather Forecast for {city_input}:")
-                    #option to print latitude and longitude for city
+                    """
+                    option to print latitude and longitude for city
                     print("-------------------------------------------------------")
                     print(f"Latitude: {lat}, Longitude: {lon}")
+                   """
                     current_date = ""
                     for forecast in forecast_data['list']:
                         forecast_time = dt.datetime.fromtimestamp(forecast['dt'])
@@ -141,7 +143,7 @@ while True:
                             wind_speed = forecast['wind']['speed']
                             humidity = forecast['main']['humidity']
                             description = forecast['weather'][0]['description']
-                            rain = forecast.get('rain', {}).get ('3h', 0)
+                            rain = forecast.get('rain', {}).get('3h', 0)
                         # print the 5 day weather forecast for that city
                             print("-------------------------------------------------------")
                             print(f"City: {city_input}")
@@ -156,81 +158,80 @@ while True:
                 break
         elif choice == '3':
             # Get current pollen data for a city
-                while True:
-                    city_name = input("Enter the city name: ")
-                    if not city_name:  # Check if the input is empty 
-                        print("Input cannot be empty. Please enter a valid city name.")
-                        continue
-                    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={api_key}"
-                    response = requests.get(url)
-                    data = response.json()
+            while True:
+                city_name = input("Enter the city name: ")
+                if not city_name:  # Check if the input is empty
+                    print("Input cannot be empty. Please enter a valid city name.")
+                    continue
+                url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={api_key}"
+                response = requests.get(url)
+                data = response.json()
             # Check if the city was found
-                    if response.status_code == 404 or not data:
-                        print("City not found. Please enter a valid city name.")
-                        continue
+                if response.status_code == 404 or not data:
+                    print("City not found. Please enter a valid city name.")
+                    continue
 
             # Extract latitude and longitued for the city
-                    if data:
-                        lat = data[0]['lat']
-                        lon = data[0]['lon']
-                        break
+                if data:
+                    lat = data[0]['lat']
+                    lon = data[0]['lon']
+                    break
             # Set up caching and retry mechanisms
-                cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-                retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-                openmeteo = openmeteo_requests.Client(session = retry_session)
+            cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+            retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+            openmeteo = openmeteo_requests.Client(session=retry_session)
 
             # Specifying the Parameters needed to make a request to the API that provides pollen data.
-                pollen_data = "https://air-quality-api.open-meteo.com/v1/air-quality"
-                params = {
-                    "latitude": lat,
-                    "longitude": lon,
-                    "current": ["alder_pollen", "birch_pollen", "grass_pollen", "mugwort_pollen", "olive_pollen", "ragweed_pollen"],
-                    "start_date": "2024-06-28",
-                    "end_date": "2024-06-28"
-                }
-                responses = openmeteo.weather_api(pollen_data, params=params)
-
-
-                response = responses[0]
+            pollen_data = "https://air-quality-api.open-meteo.com/v1/air-quality"
+            params = {
+                "latitude": lat,
+                "longitude": lon,
+                "current": ["alder_pollen", "birch_pollen", "grass_pollen", "mugwort_pollen", "olive_pollen", "ragweed_pollen"],
+                "start_date": "2024-06-28",
+                "end_date": "2024-06-28"
+            }
+            responses = openmeteo.weather_api(pollen_data, params=params)
+            response = responses[0]
 
             # Extract pollen data
-                current = response.Current()
-                current_alder_pollen = current.Variables(0).Value()
-                current_birch_pollen = current.Variables(1).Value()
-                current_grass_pollen = current.Variables(2).Value()
-                current_mugwort_pollen = current.Variables(3).Value()
-                current_olive_pollen = current.Variables(4).Value()
-                current_ragweed_pollen = current.Variables(5).Value()
-                # Print pollen data and city name
-                print("\n"*6)
-                print("-------------------------------------------------------")
-                print(f"{city_name}")
-                #option for future users to print co-ordinates for cross-checking
-                #print("-------------------------------------------------------")
-                #print(f"Latitude: {lat}, Longitude: {lon}")
-                print("-------------------------------------------------------")
-                print(f"Current alder pollen {current_alder_pollen} Grains/m³\n")
-                print(f"Current birch pollen {current_birch_pollen} Grains/m³\n")
-                print(f"Current grass pollen {current_grass_pollen} Grains/m³\n")
-                print(f"Current mugwort pollen {current_mugwort_pollen} Grains/m³\n")
-                print(f"Current olive pollen {current_olive_pollen} Grains/m³\n")
-                print(f"Current ragweed pollen {current_ragweed_pollen} Grains/m³\n")
-                print("-------------------------------------------------------")
+            current = response.Current()
+            current_alder_pollen = current.Variables(0).Value()
+            current_birch_pollen = current.Variables(1).Value()
+            current_grass_pollen = current.Variables(2).Value()
+            current_mugwort_pollen = current.Variables(3).Value()
+            current_olive_pollen = current.Variables(4).Value()
+            current_ragweed_pollen = current.Variables(5).Value()
+            # Print pollen data and city name
+            print("\n" * 6)
+            print("-------------------------------------------------------")
+            print(f"{city_name}")
+            """
+            option for future users to print co-ordinates for cross-checking
+            print("-------------------------------------------------------")
+            print(f"Latitude: {lat}, Longitude: {lon}")
+            """
+            print("-------------------------------------------------------")
+            print(f"Current alder pollen {current_alder_pollen} Grains/m³\n")
+            print(f"Current birch pollen {current_birch_pollen} Grains/m³\n")
+            print(f"Current grass pollen {current_grass_pollen} Grains/m³\n")
+            print(f"Current mugwort pollen {current_mugwort_pollen} Grains/m³\n")
+            print(f"Current olive pollen {current_olive_pollen} Grains/m³\n")
+            print(f"Current ragweed pollen {current_ragweed_pollen} Grains/m³\n")
+            print("-------------------------------------------------------")
         elif choice == '4':
             cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
             retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
             openmeteo = openmeteo_requests.Client(session=retry_session)
 
             while True:
-            # Get historical weather data for a city
+                # Get historical weather data for a city
                 city_name = input("Enter the city name: ")
                 if not city_name:  # Check if the input is empty
-                        print("Input cannot be empty. Please enter a valid city name.")
-                        continue
+                    print("Input cannot be empty. Please enter a valid city name.")
+                    continue
                 url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={api_key}"
                 response = requests.get(url)
                 data = response.json()
-            
             # Check if the city was found
                 if response.status_code == 404 or not data:
                     print("City not found. Please enter a valid city name.")
@@ -241,9 +242,9 @@ while True:
                     lon = data[0]['lon']
                     break
             while True:
-            # Date input from the user
+                # Date input from the user
                 date = input("Enter a valid date format (YYYY-MM-DD): ")
-            # Date validation
+                # Date validation
                 if not re.match(r"^\d{4}-\d{2}-\d{2}$", date):
                     print("Error: Invalid date format. Please try again.")
                     continue
@@ -271,39 +272,42 @@ while True:
             hourly_wind_speed_10m = hourly.Variables(5).ValuesAsNumpy()
 
             # Create a dictionary to store three-hourly data
-            two_hourly_data = {"Date and time": pd.date_range(
-            start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
-            end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
-            freq=pd.Timedelta(hours=2),
-            inclusive="left")
+            two_hourly_data = {
+                "Date and time": pd.date_range(
+                    start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+                    end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+                    freq=pd.Timedelta(hours=2),
+                    inclusive="left"
+                )
             }
 
             # Populate the dictionary with data, selecting every 2nd hour
-            two_hourly_data["Temperature °C"] = hourly_temperature_2m[::2] # Select every 2nd hour 
+            two_hourly_data["Temperature °C"] = hourly_temperature_2m[::2]  # Select every 2nd hour
             two_hourly_data["Relative humidity %"] = hourly_relative_humidity_2m[::2]
             two_hourly_data["Rain mm"] = hourly_rain[::2]
-            two_hourly_data["Snowfall"] = hourly_snowfall [::2]
+            two_hourly_data["Snowfall"] = hourly_snowfall[::2]
             two_hourly_data["Wind speed kph"] = hourly_wind_speed_10m[::2]
-            
+
             # Create a DataFrame from the dictionary
             two_hourly_dataframe = pd.DataFrame(data=two_hourly_data)
 
             # Print the data row by row
-            print("\n"*3)
+            print("\n" * 3)
             print("-------------------------------------------------------")
             print(f"Historical Weather Data for {city_name} on {date}:")
-            #option for future users to print co-ordinates for cross-checking
-            #print("-------------------------------------------------------")
-            #print(f"Latitude: {lat}, Longitude: {lon}")
+            """
+            option for future users to print co-ordinates for cross-checking
+            print("-------------------------------------------------------")
+            print(f"Latitude: {lat}, Longitude: {lon}")
+            """
             print("-------------------------------------------------------")
             for index, row in two_hourly_dataframe.iterrows():
                 print(row.to_string())
-                print("-------------------------------------------------------")       
+                print("-------------------------------------------------------")
             # Ask the user if they would like to clear the console
         clear_console = input("Would you like to clear the console? (yes/no): ").lower()
-        if clear_console in ['yes', 'y']:       
+        if clear_console in ['yes', 'y']:
             clear()
         while clear_console not in ['yes', 'no']:
             print("Invalid input. Please enter 'yes' or 'no'.")
             clear_console = input("Would you like to clear the console? (yes/no): ").lower()
-        
